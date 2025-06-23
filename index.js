2d0c1383db3a0d8fc5a98d25034f333868121326
Lorenzo <lorenzokaio12@gmail.com>
2025-06-23 18:06:40 -0300

    Testando conexão com o banco e buildando a API

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// importar as rotas
+const personagemRoutes = require('./routes/personagemRoutes');
+
+//Middlewares
+app.use(express.json()); // receber JSON no body
+
+//rotas
+app.use('/api', personagemRoutes);
+
+
+//Porta do servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
