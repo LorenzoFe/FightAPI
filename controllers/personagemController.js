302021e419be41934bcd9649c0124be90631b9ed
Lorenzo <lorenzokaio12@gmail.com>
2025-06-23 21:00:54 -0300

    Colocado o campo para receber a URL da imagem

--- controllers/personagemController.js
+++ controllers/personagemController.js
@@ -25,12 +25,12 @@ exports.getById = async(req, res) =>{
 // Criar personagem
 
 exports.create = async(req, res) =>{
-    const{name, tipo, especial} = req.body;
+    const{name, tipo, especial, imagem} = req.body;
     try{
         const[result] = await db.query(
-            'INSERT INTO personagem (name, tipo, especial) VALUES (?, ?, ?)', [name, tipo, especial]
+            'INSERT INTO personagem (name, tipo, especial, imagem) VALUES (?, ?, ?, ?)', [name, tipo, especial, imagem]
         );
-        res.status(201).json({id: result.insertId, name, tipo, especial});
+        res.status(201).json({id: result.insertId, name, tipo, especial, imagem});
     }catch(err){
         res.status(500).json({error: err.message});
     }
@@ -38,18 +38,20 @@ exports.create = async(req, res) =>{
 
 //atualizar personagem
 
-exports.update = async(req, res) =>{
-    const { name, tipo, especial } = req.body;
-    try{
-        const [result] = await db.query(
-            'UPDATE personagem SET name = ?, tipo = ?, especial = ? WHERE id = ?', [name, tipo, especial, req.params.id]
-        );
-        if(result.affectedRows === 0 ) return res.status(404).json({ message:'Personagem não encontrado'});
-        res.json({ id: req.params.id, name, tipo, especial});
-    } catch(err){
-        res.status(500).json({error: err.message});
-    }
+exports.update = async (req, res) => {
+  const { name, tipo, especial, imagem } = req.body;
+  try {
+    const [result] = await db.query(
+      'UPDATE personagem SET name = ?, tipo = ?, especial = ?, imagem = ? WHERE id = ?',
+      [name, tipo, especial, imagem, req.params.id] // ← Agora com 5 valores certinhos
+    );
+    if (result.affectedRows === 0) return res.status(404).json({ message: 'Personagem não encontrado' });
+    res.json({ id: req.params.id, name, tipo, especial, imagem });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
+
 
 //Deletar personagem
 
